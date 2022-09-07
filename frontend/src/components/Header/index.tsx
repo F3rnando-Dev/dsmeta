fe7c6d113e6bf3fe37f212f31f10f321a35ee844
Fernando Santana <fernando_92-@hotmail.com>
2022-09-06 23:30:27 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="logo_DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por
+                        <a href="https://www.instagram.com/entendinadaoficial/">@fernando.dev</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
